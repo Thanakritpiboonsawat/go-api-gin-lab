625a05e709a22e97a1639483ebb22357b20c2578
Thanakrit Piboonsawat <thanakrit.pib@dome.tu.ac.th>
2026-02-07 15:47:41 +0700

    lab3

---
+++ services/student_service.go
@@ -0,0 +1,22 @@
+package services
+
+import (
+	"example.com/student-gin-api/models"
+	"example.com/student-gin-api/repositories"
+)
+
+type StudentService struct {
+	Repo *repositories.StudentRepository
+}
+
+func (s *StudentService) GetStudents() ([]models.Student, error) {
+	return s.Repo.GetAll()
+}
+
+func (s *StudentService) GetStudentByID(id string) (*models.Student, error) {
+	return s.Repo.GetByID(id)
+}
+
+func (s *StudentService) CreateStudent(student models.Student) error {
+	return s.Repo.Create(student)
+}
